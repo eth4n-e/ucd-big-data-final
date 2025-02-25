8428347b9a3c6e80dd7f2f54a60fe216c12e9d34
eth4n-e <etep9497@colorado.edu>
2025-02-25 20:54:20 +0000

    updated create.sql script

--- sql/create.sql
+++ sql/create.sql
@@ -1,6 +1,9 @@
-CREATE DATABASE big-five IF NOT EXISTS;
+/* Create and begin using database */
+CREATE DATABASE IF NOT EXISTS big_five;
+USE big_five;
 
-CREATE TABLE users (
+/* Create tables */
+CREATE TABLE IF NOT EXISTS user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     dateload DATETIME, /* Timestamp when survey started */
     screenw INT, /* Screen width in pixels */
@@ -10,17 +13,21 @@ CREATE TABLE users (
     endelapse INT, /* Number of seconds spent on finalization page */
     IPC INT, /* Number of responses from this IP */
     country VARCHAR(2), 
-    lat FLOAT, /* Approximate latitude of user */
-    long FLOAT, /* Approximate longitude of user */
+    latitude FLOAT, /* Approximate latitude of user */
+    longitude FLOAT /* Approximate longitude of user */
 );
-CREATE TABLE questions (
+
+CREATE TABLE IF NOT EXISTS question (
     question_id INT PRIMARY KEY, 
     question_text VARCHAR(255),
     trait_category VARCHAR(3) /*3 letter code representing the trait*/
 );
-CREATE TABLE responses (
+
+CREATE TABLE IF NOT EXISTS response (
     response_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT FOREIGN KEY,
-    question_id INT FOREIGN KEY,
-    response_value TINYINT, /* User's response on a 1-5 scale */
+    user_id INT,
+    question_id INT, /* Must create columns before using as foreign key */
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (question_id) REFERENCES question(question_id),
+    response_value TINYINT /* User's response on a 1-5 scale */
 );
